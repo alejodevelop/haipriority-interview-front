b9ea810c70e77ce1deb30f7e7e2a445d76170fed
alejo <alejodevelop@gmail.com>
2024-09-15 15:59:51 -0500

    FEATURE: navbar styled and fit to content

--- src/app/shared/components/layout/navbar/navbar.component.ts
+++ src/app/shared/components/layout/navbar/navbar.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {MatDrawer, MatDrawerContainer, MatSidenavModule} from "@angular/material/sidenav";
+import {MatButton, MatButtonModule} from "@angular/material/button";
+import {FooterComponent} from "../footer/footer.component";
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     RouterLinkActive,
-    RouterLink
+    RouterLink,
+    MatDrawerContainer,
+    MatButton,
+    MatSidenavModule,
+    MatButtonModule,
+    RouterOutlet,
+    FooterComponent,
+    MatIconModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  isExpanded = false;
+
+  toggleDrawer(drawer: MatDrawer) {
+    drawer.toggle();
+    this.isExpanded = !this.isExpanded;
+  }
 
 }
